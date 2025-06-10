a9669b348f02acd916acbabdb473af09aff0d9fd
vijay <digitalxvijay@gmail.com>
2025-06-10 10:45:05 +0530

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
 const Header = () => {
+  const navigate = useNavigate();
+  const user = false;
+
   return (
-    <nav>
-      <Link>
+    <nav className="flex justify-between items-center">
+      <Link to="/">
         <img
           src="https://cdn-icons-png.flaticon.com/512/3214/3214679.png"
           alt="trimmer logo"
           className="h-15"
         />
       </Link>
+      <div>
+        {
+          
+        }
+        <Button onClick={() => navigate("/auth")}>Login</Button>
+      </div>
     </nav>
   );
 };
